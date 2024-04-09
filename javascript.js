f9120c867de9235f4d3fb84fd8bf909bf2eeb201
Ukam <ukam@Xubuntu.myguest.virtualbox.org>
2024-04-09 22:08:18 +0000

    Add index.html and javascript.js

--- javascript.js
+++ javascript.js
@@ -1,53 +1,58 @@
 
 const myLibrary = [];
+const tbody = document.querySelector('tbody');
 
-function Book(title, author, pages, bookRead) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.bookRead = bookRead;
     
 }
 
-  
-function addBookToLibrary(ti, au, pa, bR) {
-    const newBook = new Book(ti,au,pa,bR);
+ 
+function addBookToLibrary(ti, au, pa) {
+    const newBook = new Book(ti,au,pa);
     myLibrary.push(newBook);
     return myLibrary;
 }
 
 const tab = document.querySelector('.tab');
 const columns = ['Title','Author(s)','Pages','Status'];
-//var posts_array = JSON.parse(xhr.responseText);
 
 
 
 function appendToCard(arry) {
+
     for (let i=0; i < arry.length; i++) {
         let row = tab.insertRow(-1);
         let objArray = arry[i];
         let arrKeys = Object.values(objArray);
         
-        for (let j=0; j< columns.length; j++) {
+        for (let j=0; j< columns.length-1; j++) {
             let cell = row.insertCell(-1);
             cell.className = columns[j];
             cell.innerHTML = arrKeys[j];
             
-            
-            
         }
+        let cell = row.insertCell(-1);
+        cell.className = columns[3];
+        const readToggle = document.createElement("button");
+        readToggle.textContent = "read";
+
+        readToggle.addEventListener('click',() => {
+            if (readToggle.textContent =="read") {
+                readToggle.textContent = "unread";
+            } else {
+                readToggle.textContent = "read";
+                }
+        })
+
+        cell.append(readToggle)
+        
     }
+   
 }
 
-
-//const theHobbit = addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, 'nO');
-
-console.log(myLibrary);
-
-//const theWoman = addBookToLibrary('The Woman', 'Sr. Sarah', 300, 'yes');
-//const Brain = addBookToLibrary('Brain', 'P.H Jas', 100, 'yes');
-
-let fer = appendToCard(myLibrary);
 
 const myForm = document.querySelector('#myForm');
 const butN = document.querySelector('button');
@@ -59,41 +64,44 @@ myForm.addEventListener("submit", function (event) {
     const nuM = document.querySelector("#numPa").value;
     const yS = document.querySelector("#yes").checked;
 
-    let yN = '';
-    if (yS == true) yN = 'read';
-    else yN= 'unread';
-    //const noS = document.querySelector("#no").value;
-
-    let fer2 = addBookToLibrary(titl, auth, nuM, yN);
+   
+    let fer2 = addBookToLibrary(titl, auth, nuM);
     console.log(fer2);
     appendToCard(fer2);
-    myLibrary = myLibrary.splice(0, myLibrary.length);
+    
+
+
+    myLibrary.splice(0, myLibrary.length);
+    
     
 });
 
-function changeStat() {
-    let toggle_read = document.createElement('button')
-        toggle_read.className = "toggle_read"
-        const yS = document.querySelector("#yes").checked;
-        if (yS){
-            toggle_read.textContent = "unread"
-        }
-        else{
-            toggle_read.textContent = "read"
-        }
-        toggle_read.addEventListener("click", ()=>{
-            if (yS){
-                yS= false
-            }
-            else {
-                yS = true
-            }
-        })
-        //book_div.append(toggle_read)
-        //shielf.append(book_div)
-    
-}
 
-//let fer1 = appendToCard(myLibrary);
-//console.log(fer);
+const theHobbit = addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295);
+let fer = appendToCard(myLibrary);
+myLibrary.splice(0, 1);
+
+
+
+const books = document.getElementById('books');
+
+const bookDiv = document.createElement("div");
+const deleteBtn = document.createElement("button");
+deleteBtn.textContent = "delete book?"; 
+bookDiv.append(deleteBtn);
+books.append(bookDiv);
+deleteBtn.addEventListener('click', () => {
+    let i = +prompt("starting from 1, what book index number do you want to delete?",)
+    tab.deleteRow(i);
+            
+});
+
+
+const clr = document.querySelector('#clr');
+
+clr.addEventListener('click', ()=> {
+    myForm.reset();
+})
+
+
 
